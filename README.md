113ea882197a3f7048b4eb7bca85a863432077c5
mderoir <mderoir@PC-MDR.stramatel.local>
2024-02-26 09:45:41 +0100

    first commit: install.sh unzip and create a checksum

---
+++ README.md
@@ -0,0 +1,1 @@
+wget -q -O - https://raw.githubusercontent.com/StramatelBE/G552_BUILDS/master/install.sh | bash
---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+echo -e "Starting \033[35m setup \033[0m process..."
+
+GITHUB_REPOSITORY=https://github.com/StramatelBE/G552_BUILDS.git
+WORKDIR=server_builds
+BUILD_FILE=G552_BUILDS.zip
+
+# Initialize progress
+TOTAL_STEPS=8
+CURRENT_STEP=-1
+
+function print_progress {
+    CURRENT_STEP=$((CURRENT_STEP + 1))
+    PERCENT=$(( (CURRENT_STEP * 100) / TOTAL_STEPS ))
+    echo -ne "Progress: ["
+    for ((i = 0; i < (PERCENT / 10); i++)); do echo -ne "\033[33m#\033[0m"; done
+    for ((i = (PERCENT / 10); i < 10; i++)); do echo -ne "\033[31m-\033[0m"; done
+    echo -ne "] \033[32m $PERCENT\033[0m% - \033[36m $1 \033[0m \r"
+    sleep 1 # Simulating time taken for the step
+}
+
+#SOFTWARE UPDATE
+print_progress "Updating software packages..."
+sudo apt update >/dev/null 2>&1
+sudo apt upgrade -y >/dev/null 2>&1
+
+#BASIC UTILITIES
+print_progress "Installing basic utilities...        "
+sudo apt install -y vim curl wget git zip >/dev/null 2>&1
+
+#CLONE REPOSITORY
+print_progress "Cloning repository...               "
+cd ~
+git clone $GITHUB_REPOSITORY $WORKDIR >/dev/null 2>&1
+
+#NODE INSTALL
+print_progress "Installing Node.js...               "
+bash ~/$WORKDIR/scripts/setup/node_install.sh >/dev/null 2>&1
+npm install -g serve >/dev/null 2>&1
+
+#CHECKSUM CREATION
+echo $(sha256sum "~/$WORKDIR/$BUILD_FILE" >> ~/$WORKDIR/checksum)
+
+#UNZIPPING 
+print_progress "Unzipping...                      "
+unzip ~/$WORKDIR/$BUILD_FILE >/dev/null 2>&1
+
+#RUN
+print_progress "Running application...              "
+# bash ~/$WORKDIR/scripts/run/run.sh >/dev/null 2>&1
+
+#SERVICE
+print_progress "Initializing services...            "
+# bash ~/$WORKDIR/scripts/services/services_init.sh >/dev/null 2>&1
+
+#FINISHING
+print_progress "Installation complete !            "
+
+echo -ne '\n'
+echo -e "\033[34m#\033[0m#\033[31m# \033[34mPROJECT \033[0m SUCCESSFULLY \033[31m INITIALIZED \033[0m \033[34m#\033[0m#\033[31m#"
