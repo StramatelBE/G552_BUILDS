d2058ee8e16559c660be8901e05eab2cad70a03f
mderoir <mderoir@PC-MDR.stramatel.local>
2024-02-26 14:28:45 +0100

    removed dev/null redirection

--- install.sh
+++ install.sh
@@ -49,21 +49,18 @@ VERSION=$(cat ~/$WORKDIR/builds/selected_version)
 BUILD_FILE=G552_BUILD_$VERSION.zip
 #UNZIPPING 
 print_progress "Unzipping...                      "
-echo "WORKDIR : $WORKDIR"
-echo "G552 Version : $SELECTED_VERSION"
-echo "File : $BUILD_FILE"
 unzip ~/$WORKDIR/$SELECTED_VERSION/$BUILD_FILE 
-clear
+#clear
 
 #RUN
 print_progress "Running application...              "
 bash ~/$WORKDIR/scripts/run/run.sh 
-clear
+#clear
 
 #SERVICE
 print_progress "Initializing services...            "
 # bash ~/$WORKDIR/scripts/services/services_init.sh
-clear
+#clear
 
 #FINISHING
 print_progress "Installation complete !            "
@@ -71,26 +68,4 @@ print_progress "Installation complete !            "
 
 echo -ne '\n'
 echo -e "\033[34m#\033[0m#\033[31m# \033[34mPROJECT \033[0m SUCCESSFULLY \033[31m INITIALIZED \033[0m \033[34m#\033[0m#\033[31m#"
-
-echo -ne '\n'
-echo -e "Do you want to reboot now? (Y/n) The system will automatically reboot in 30 seconds if no input is provided: "
-
-# Start a background process for the countdown
-{
-    for ((i=30; i>0; i--)); do
-        sleep 1 &
-        printf "\rRebooting in $i seconds... Press 'n' to cancel.    "
-        wait
-    done
-    echo -e "\nRebooting now..."
-    sudo reboot
-} &
-
-# Wait for user input in the foreground and cancel the background job if 'n' is pressed
-read -t 30 user_input
-if [[ $user_input =~ ^[Nn]$ ]]; then
-    # Kill the background job (the countdown and automatic reboot)
-    kill $!
-    echo -e "\nReboot cancelled."
-fi
 
